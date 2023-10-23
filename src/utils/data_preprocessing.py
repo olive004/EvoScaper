bc59c5f22a5bef06d18ce03e31d6b7aa618d632a
olive004 <olivia.gallup@gmail.com>
2023-10-23 16:19:14 +0100

    convert txt to csv specific format

--- src/utils/data_preprocessing.py
+++ src/utils/data_preprocessing.py
@@ -70,3 +70,27 @@ def proc_info(info: pd.DataFrame, include_log: bool = True):
         info[[c + '_logm' for c in numerical_cols]] = mutation_log[numerical_cols]
 
     return info, num_group_cols, num_bs_cols, numerical_cols, key_cols, bs_range_cols
+
+
+def txt_to_csv(input_file: str, output_file: str):
+    """ GCG """
+    
+    import csv
+
+    # Open the input and output files
+    with open(input_file, 'r') as txt_file, open(output_file, 'w', newline='') as csv_file:
+        # Create a CSV writer
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        # Write the header row to the CSV file
+        header = next(txt_file).strip().split('\t')
+        csv_writer.writerow(header)
+
+        # Process the rest of the lines in the text file
+        for line in txt_file:
+            # Split the line by tab character
+            data = line.strip().split('\t')
+            # Write the data to the CSV file
+            csv_writer.writerow(data)
+
+    print("Conversion complete. CSV file saved as", output_file)
