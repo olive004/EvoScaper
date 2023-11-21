44e9c21c40de1aa40748b69c2f044b4b9c561023
olive004 <kempf.idris@gmail.com>
2023-11-21 10:12:38 +0000

    ran notebook on orthogonality

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -6,9 +6,9 @@ sudo docker pull quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
 sudo docker pull nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
 
 # If image not built yet
-sudo docker build -t genetic_glitch:latest docker_unix
+sudo docker build -t genetic_glitch:latest docker
 
-cp ./requirements.txt ./docker_unix
+cp ./requirements.txt ./docker
 
 sudo docker create -it \
 --rm \
