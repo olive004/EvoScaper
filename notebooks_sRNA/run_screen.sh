12da785810adbaef134ec641f3ea735451b6aff7
olive004 <olivia.gallup@gmail.com>
2024-01-18 17:51:29 +0000

    running screen with more segments

--- notebooks_sRNA/run_screen.sh
+++ notebooks_sRNA/run_screen.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Number of iterations
-num_iterations=1
+num_iterations=24
 file_to_modify="run_iterate_script.sh"
-cp $file_to_modify "temp_mod_file.sh"
 
 # Loop to run each iteration within a screen session
-for ((i=0; i<=$num_iterations; i++)); do
+for ((i=0; i<$num_iterations; i++)); do
     screen_name="screen_$i"
 
     screen -dmS $screen_name
+
+    cp $file_to_modify "temp_mod_file.sh"
 
     sed -i "s/i=0/i=$i/" "temp_mod_file.sh"
 
@@ -17,10 +18,11 @@ for ((i=0; i<=$num_iterations; i++)); do
     screen -S "$screen_name" -X stuff "bash -c '
         # Within the screen session, put the rest of your loop logic here
         # For example, you can execute your modified notebook script
-        cd notebooks_sRNA
-        echo \"1212\" | sudo bash temp_mod_file.sh
+        # cd notebooks_sRNA
+        bash temp_mod_file.sh
     '$(printf \\r)"
 
+    sleep 1
     echo "Started loop $i in screen $screen_name"
 done
 
