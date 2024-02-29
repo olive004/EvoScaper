8c8d35e198e1d69f592fdad3f29ab68a86405f66
olive004 <olivia.gallup@gmail.com>
2024-02-29 09:37:51 +0000

    visualisations

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -3,7 +3,8 @@
 
 sudo docker pull quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
 # sudo docker run --rm -it --entrypoint bash quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
-sudo docker pull nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+# sudo docker pull nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+sudo docker pull nvidia/cuda:12.2.2-cudnn8-runtime-ubuntu22.04
 
 # If image not built yet
 sudo docker build -t genetic_glitch:latest docker
@@ -13,6 +14,12 @@ cp ./requirements.txt ./docker
 sudo docker create -it \
 --rm \
 --gpus all \
+--device /dev/nvidia0:/dev/nvidia0 \
+--device /dev/nvidia-modeset:/dev/nvidia-modeset \
+--device /dev/nvidia-caps:/dev/nvidia-caps \
+--device /dev/nvidia-uvm:/dev/nvidia-uvm \
+--device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools \
+--device /dev/nvidiactl:/dev/nvidiactl \
 --name gcg \
 --mount type=bind,source="$(pwd)",target=/workdir \
 genetic_glitch:latest
