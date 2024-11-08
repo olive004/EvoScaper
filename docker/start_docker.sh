91e50b41d4451d313c3b4723141bc7a6fb8f9c8b
olive004 <olivia.gallup@gmail.com>
2024-11-08 19:14:19 +0000

    update docker

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -7,25 +7,32 @@ sudo nvidia-container-cli --load-kmods info
 sudo docker pull quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
 # sudo docker run --rm -it --entrypoint bash quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
 # sudo docker pull nvidia/cuda:12.6.0-cudnn-devel-ubuntu22.04
-sudo docker pull nvidia/cuda:12.2.2-cudnn8-runtime-ubuntu22.04
+# sudo docker pull nvidia/cuda:12.2.2-cudnn8-runtime-ubuntu22.04
+sudo docker pull nvidia/cuda:12.0.0-cudnn8-devel-ubuntu22.04
 
 # If image not built yet
 sudo docker build -t genetic_glitch:latest docker
 
 cp ./requirements.txt ./docker
 
+# sudo docker create -it \
+# --rm \
+# --gpus all \
+# --device /dev/nvidia0:/dev/nvidia0 \
+# --device /dev/nvidia-modeset:/dev/nvidia-modeset \
+# --device /dev/nvidia-caps:/dev/nvidia-caps \
+# --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+# --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools \
+# --device /dev/nvidiactl:/dev/nvidiactl \
+# --name evo \
+# --mount type=bind,source="$(pwd)",target=/workdir \
+# genetic_glitch:latest
 sudo docker create -it \
 --rm \
 --gpus all \
---device /dev/nvidia0:/dev/nvidia0 \
---device /dev/nvidia-modeset:/dev/nvidia-modeset \
---device /dev/nvidia-caps:/dev/nvidia-caps \
---device /dev/nvidia-uvm:/dev/nvidia-uvm \
---device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools \
---device /dev/nvidiactl:/dev/nvidiactl \
---name gcg \
+--name evo \
 --mount type=bind,source="$(pwd)",target=/workdir \
 genetic_glitch:latest
-sudo docker container start gcg
-sudo docker exec -it gcg /bin/bash 
-# sudo docker container stop gcg
+sudo docker container start evo
+sudo docker exec -it evo /bin/bash 
+# sudo docker container stop evo
