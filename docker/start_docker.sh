b5fd0a15599967c9935f7d94383ef39c134cda8b
olive004 <olivia.gallup@gmail.com>
2024-11-20 12:05:42 +0000

    merge

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -8,12 +8,20 @@ sudo docker pull quay.io/biocontainers/intarna:3.4.1--pl5321hdcf5f25_0
 # sudo docker run --rm -it --entrypoint bash quay.io/biocontainers/intarna:3.4.1--pl5321hdcf5f25_0
 # sudo docker pull nvidia/cuda:12.6.0-cudnn-devel-ubuntu22.04
 # sudo docker pull nvidia/cuda:12.2.2-cudnn8-runtime-ubuntu22.04
-sudo docker pull nvidia/cuda:12.0.0-cudnn8-devel-ubuntu22.04
 
 # If image not built yet
-sudo docker build -t genetic_glitch:latest docker
+if [ "$(id -un)" != "wadh6511" ]; then
+    sudo docker pull nvidia/cuda:12.0.0-cudnn8-devel-ubuntu22.04
+    source_directory="docker_mufasa"
+else
+    sudo docker pull nvidia/cuda:12.6.0-cudnn-devel-ubuntu22.04
+    source_directory="docker"
+fi
 
-cp ./requirements.txt ./docker
+sudo docker build -t genetic_glitch:latest $source_directory
+
+cp ./requirements.txt ./$source_directory
+
 
 # sudo docker create -it \
 # --rm \
@@ -37,5 +45,6 @@ sudo docker container start evo
 
 # sudo docker exec -it evo cd /usr/lib/install_requirements && pip install -r ./requirements.txt
 # sudo docker exec -it evo pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+sudo docker exec -it evo bash docker/post_install.sh
 sudo docker exec -it evo /bin/bash 
 # sudo docker container stop evo
