48a0cd89eb731c8f3760bd4f550d5c3ba617356e
olive004 <olivia.gallup@gmail.com>
2024-11-20 10:34:25 +0000

    update installation

--- docker/post_install.sh
+++ docker/post_install.sh
@@ -30,16 +30,14 @@ pip install -r requirements.txt
 pip install jax==0.4.29
 pip install jaxlib==0.4.29+cuda12.cudnn91 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html 
 pip install -U chex
-# pip install git+https://github.com/Steel-Lab-Oxford/core-bioreaction-simulation.git@599990dcac56e7678f45269d2fc9df736d25f356#egg=bioreaction
+# pip install git+https://github.com/Steel-Lab-Oxford/core-bioreaction-simulation.git@f903c39872de43e28b56653efda689bb082cb592#egg=bioreaction
 # pip install git+https://github.com/olive004/synbio_morpher.git@bc7aaf284fcf5b10abf591f5f2cf6c898f45861f#egg=synbio_morpher
-# pip install -e src/bioreaction
 
-if [ -d "src/bioreaction" ]; then
+if [ -d "src/core-bioreaction-simulation" ]; then
     echo "Directory src/bioreaction exists."
 else
     cd src
     git clone https://github.com/Steel-Lab-Oxford/core-bioreaction-simulation.git
-    rm src/bioreaction
     cd ..
 fi
-pip install -e core-bioreaction-simulation/src/bioreaction
+pip install -e src/core-bioreaction-simulation
