a5aff68320c01653079b72e151623599fd322aff
olive004 <olivia.gallup@gmail.com>
2025-01-28 16:31:21 +0000

    simulate circuits

---
+++ src/evoscaper/scripts/simulate_circuits.py
@@ -0,0 +1,110 @@
+
+
+import numpy as np
+import os
+
+from synbio_morpher.utils.data.data_format_tools.common import load_json_as_dict
+from evoscaper.utils.simulation import setup_model, make_rates, prep_sim, sim, prep_cfg
+from evoscaper.utils.math import make_batch_symmetrical_matrices
+from synbio_morpher.utils.data.fake_data_generation.energies import generate_energies
+from synbio_morpher.utils.data.data_format_tools.common import load_json_as_dict, write_json
+
+
+def save(returns_kwrgs, top_write_dir):
+    for i, l in returns_kwrgs.items():
+        ext = os.path.splitext(l)
+        if ext == 'npy':
+            np.save(os.path.join(top_write_dir, l), i)
+        elif ext == 'json':
+            write_json(i, os.path.join(top_write_dir, 'analytics.json'))
+    print(top_write_dir)
+
+
+def simulate_interactions(interactions, input_species, config):
+
+    interactions_reshaped = make_batch_symmetrical_matrices(
+        interactions.reshape(-1, interactions.shape[-1]), side_length=len(input_species))
+
+    model_brn, qreactions, ordered_species, postproc = setup_model(
+        interactions_reshaped, config, input_species)
+
+    forward_rates, reverse_rates = make_rates(
+        config['x_type'], interactions_reshaped, postproc)
+
+    (signal_onehot, signal_target, y00, t0, t1, dt0, dt1, stepsize_controller, save_steps, max_steps, forward_rates, reverse_rates) = prep_sim(
+        config['signal_species'], qreactions, interactions_reshaped, config, forward_rates, reverse_rates)
+
+    #
+    threshold = 0.005  # config['simulation']['threshold_steady_states']
+    t1 = 500
+    total_time = 30000
+    #
+
+    print('Starting sim')
+    analytics, ys, ts, y0m, y00s, ts0 = sim(y00, forward_rates[0], reverse_rates,
+                                            qreactions,
+                                            signal_onehot, signal_target,
+                                            t0, t1, dt0, dt1,
+                                            save_steps, max_steps,
+                                            stepsize_controller,
+                                            threshold=threshold,
+                                            total_time=total_time)
+    analytics['Log sensitivity'] = np.log10(
+        analytics['sensitivity_wrt_species-6'])
+    analytics['Log precision'] = np.log10(analytics['precision_wrt_species-6'])
+
+    return analytics, ys, ts, y0m, y00s, ts0
+
+
+def make_inputs(species_count, system_type):
+    return [f'{system_type}_{i}' for i in range(species_count)]
+
+
+def main(top_write_dir, cfg_path=None):
+
+    if cfg_path is None:
+        config = {
+            'signal_species': ['RNA_0'],
+            'x_type': 'energies',
+            'include_prod_deg': False,
+            'system_type': 'RNA',
+            'molecular_params': {
+                'avg_mRNA_per_cell': 100,
+                'cell_doubling_time': 1200,
+                'creation_rate': 2.35,
+                'starting_copynumbers': 100,
+                'degradation_rate': 0.01175,
+                'association_binding_rate': 1000000
+            },
+            'circuit_generation': {
+                'repetitions': 12000,
+                'species_count': 3,
+                'sequence_length': 20,
+                'generator_protocol': 'random',
+                'proportion_to_mutate': 0.5,
+                'template': None,
+                'seed': 9
+            },
+            'simulation': {
+                't0': 0,
+                't1': 500,
+                'dt0': 0.1,
+                'dt1': 0.1,
+                'threshold_steady_states': 0.005,
+                'total_time': 30000
+            },
+        }
+    else:
+        config_bio = load_json_as_dict(cfg_path)
+        config = config_bio['base_configs_ensemble']['generate_species_templates']
+        config.update(config_bio['base_configs_ensemble']
+                      ['mutation_effect_on_interactions_signal'])
+
+    input_species = make_inputs(config['circuit_generation']['species_count'], config['system_type'])
+    config = prep_cfg(config, input_species)
+    interactions = get_interactions(config['circuit_generation'])
+
+    os.make_dirs(top_write_dir, exist_ok=True)
+
+    analytics_perturbed, ys, ts, y0m, y00s, ts0 = simulate_interactions(
+        interactions, input_species, config)
--- src/evoscaper/scripts/verify.py
+++ src/evoscaper/scripts/verify.py
@@ -2,152 +2,23 @@
 
 import os
 from typing import List
-from synbio_morpher.srv.io.manage.script_manager import script_preamble
-from synbio_morpher.srv.parameter_prediction.simulator import RawSimulationHandling
-from synbio_morpher.utils.results.analytics.timeseries import generate_analytics
-from synbio_morpher.utils.common.setup import prepare_config, expand_config, expand_model_config
-from synbio_morpher.utils.data.data_format_tools.common import load_json_as_dict
-from synbio_morpher.utils.results.analytics.naming import get_true_interaction_cols
 from synbio_morpher.utils.results.analytics.timeseries import calculate_adaptation
-from synbio_morpher.utils.misc.type_handling import flatten_listlike, get_unique
-from synbio_morpher.utils.modelling.deterministic import bioreaction_sim_dfx_expanded
-from synbio_morpher.utils.modelling.solvers import simulate_steady_states, make_stepsize_controller
 from synbio_morpher.utils.results.result_writer import ResultWriter
-from bioreaction.model.data_tools import construct_model_fromnames
-from bioreaction.model.data_containers import BasicModel, QuantifiedReactions
-from functools import partial
+from synbio_morpher.srv.io.manage.script_manager import script_preamble
 
 import numpy as np
 import jax
-import diffrax as dfx
 import pandas as pd
 
 from evoscaper.model.sampling import sample_reconstructions
 from evoscaper.utils.dataclasses import DatasetConfig, ModelConfig, NormalizationSettings
-from evoscaper.utils.math import make_batch_symmetrical_matrices, make_symmetrical_matrix_from_sequence_nojax
+from evoscaper.utils.math import make_batch_symmetrical_matrices
 from evoscaper.utils.normalise import DataNormalizer
+from evoscaper.utils.simulation import setup_model, make_rates, prep_sim, sim, prep_cfg
 from evoscaper.utils.visualise import vis_sampled_histplot
 
 
 jax.config.update('jax_platform_name', 'gpu')
-
-
-def update_species_simulated_rates(ordered_species: list, forward_interactions, reverse_interactions, model: BasicModel, qreactions: QuantifiedReactions):
-    for i, r in enumerate(model.reactions):
-        if len(r.input) == 2:
-            model.reactions[i].forward_rate = forward_interactions[
-                ordered_species.index(r.input[0]), ordered_species.index(r.input[1])]
-
-            model.reactions[i].reverse_rate = reverse_interactions[
-                ordered_species.index(r.input[0]), ordered_species.index(r.input[1])]
-    qreactions.reactions = qreactions.init_reactions(
-        model)
-    return model, qreactions
-
-
-def compute_analytics(y, t, labels, signal_onehot):
-    y = np.swapaxes(y, 0, 1)
-
-    analytics_func = partial(
-        generate_analytics, time=t, labels=labels,
-        signal_onehot=signal_onehot, signal_time=t[1],
-        ref_circuit_data=None)
-    return analytics_func(data=y, time=t, labels=labels)
-
-
-def make_rates(x_type, fake_circuits_reshaped, postproc):
-    if x_type == 'energies':
-        eqconstants, (forward_rates, reverse_rates) = postproc(
-            fake_circuits_reshaped)
-    elif x_type == 'binding_rates_dissociation':
-        reverse_rates = fake_circuits_reshaped
-        # eqconstants = forward_rates[0, 0, 0] / reverse_rates
-    else:
-        raise ValueError(f'Unknown x_type {x_type}')
-    return forward_rates, reverse_rates
-
-
-def sim(y00, forward_rates, reverse_rates,
-        qreactions,
-        signal_onehot, signal_target,
-        t0, t1, dt0, dt1,
-        save_steps, max_steps,
-        stepsize_controller,
-        dt1_factor=5,
-        threshold=0.01,
-        total_time=None):
-    """ Concentrations should be in the form [circuits, time, species] """
-
-    rate_max = np.max([np.max(forward_rates),
-                       np.max(reverse_rates)])
-    dt0 = np.min([1 / (5 * rate_max), dt0])
-    dt1 = dt1_factor * dt0
-    total_time = t1 - t0 if total_time is None else total_time
-
-    sim_func = jax.jit(jax.vmap(partial(bioreaction_sim_dfx_expanded,
-                                t0=t0, t1=t1, dt0=dt0,
-                                signal=None, signal_onehot=None,
-                                inputs=qreactions.reactions.inputs,
-                                outputs=qreactions.reactions.outputs,
-                                forward_rates=forward_rates,
-                                solver=dfx.Tsit5(),
-                                saveat=dfx.SaveAt(
-                                    ts=np.linspace(t0, t1, save_steps)),
-                                max_steps=max_steps,
-                                stepsize_controller=make_stepsize_controller(
-                                    t0=t0, t1=t1, dt0=dt0, dt1=dt1, choice=stepsize_controller),
-                                # stepsize_controller=make_piecewise_stepcontrol(
-                                #     t0=t0, t1=t1, dt0=dt0, dt1=dt1)
-                                )))
-
-    y00s, ts0 = simulate_steady_states(y0=y00, total_time=t1-t0, sim_func=sim_func, t0=t0,
-                                       t1=t1, threshold=threshold, reverse_rates=reverse_rates, disable_logging=True)
-    y0 = np.array(y00s[:, -1, :]).reshape(y00.shape)
-    print('Steady states found. Now calculating signal response')
-
-    # Signal
-    y0m = y0 * ((signal_onehot == 0) * 1) + y00 * signal_target * signal_onehot
-    ys, ts = simulate_steady_states(y0m, total_time=t1-t0, sim_func=sim_func, t0=t0,
-                                    t1=t1, threshold=threshold, reverse_rates=reverse_rates, disable_logging=True)
-    ys = np.concatenate([y0m, ys.squeeze()[:, :-1, :]], axis=1)
-
-    analytics = jax.vmap(partial(compute_analytics, t=ts, labels=np.arange(
-        ys.shape[-1]), signal_onehot=signal_onehot))(ys)
-
-    return analytics, ys, ts, y0m, y00s, ts0
-
-
-def setup_model(fake_circuits_reshaped, config_bio: dict, input_species: List[str]):
-    model_brn = construct_model_fromnames(
-        sample_names=input_species, include_prod_deg=config_bio['include_prod_deg'])
-    ordered_species = sorted(get_unique(flatten_listlike(
-        [r.input for r in model_brn.reactions if r.output])))
-    for i in range(len(model_brn.reactions)):
-        model_brn.reactions[i].forward_rate = 0
-        model_brn.reactions[i].reverse_rate = 0
-        if (not model_brn.reactions[i].input) and config_bio['include_prod_deg']:
-            model_brn.reactions[i].forward_rate = config_bio['molecular_params'].get(
-                'creation_rate')
-        elif (not model_brn.reactions[i].output) and config_bio['include_prod_deg']:
-            model_brn.reactions[i].forward_rate = config_bio['molecular_params'].get(
-                'degradation_rate')
-
-    qreactions = QuantifiedReactions()
-    qreactions.init_properties(
-        model_brn, config_bio.get('starting_concentration'))
-
-    quantities = np.array(
-        [r.quantity for r in qreactions.reactants if r.species.name in input_species])
-    postproc = RawSimulationHandling(
-        config_bio['interaction_simulator']).get_postprocessing(initial=quantities)
-
-    # Update qreactions (using dummy rates)
-    eqconstants, (a_rates, d_rates) = postproc(
-        np.array(fake_circuits_reshaped[0]))
-    model_brn, qreactions = update_species_simulated_rates(
-        ordered_species, a_rates, d_rates, model_brn, qreactions)
-
-    return model_brn, qreactions, ordered_species, postproc
 
 
 def save(data_writer, analytics, ys, ts, y0m, fake_circuits, sampled_cond):
@@ -160,36 +31,6 @@ def save(data_writer, analytics, ys, ts, y0m, fake_circuits, sampled_cond):
                        out_name='fake_circuits')
     data_writer.output(data=sampled_cond, out_type='npy',
                        out_name='sampled_cond')
-
-
-def prep_sim(signal_species, qreactions, fake_circuits_reshaped, config_bio,
-             forward_rates, reverse_rates):
-
-    signal_onehot = np.where(
-        [r.species.name in signal_species for r in qreactions.reactants], 1, 0)
-
-    def make_flat_triangle(matrices):
-        return np.array(list(map(lambda i: i[np.triu_indices(n=matrices.shape[-1])], matrices)))
-    forward_rates, reverse_rates = make_flat_triangle(
-        forward_rates), make_flat_triangle(reverse_rates)
-    signal_target = config_bio['signal']['function_kwargs']['target']
-    y00 = np.repeat(np.array([r.quantity for r in qreactions.reactants])[
-                    None, None, :], repeats=len(fake_circuits_reshaped), axis=0)
-    t0, t1, dt0, dt1, stepsize_controller = config_bio['simulation']['t0'], config_bio['simulation'][
-        't1'], config_bio['simulation']['dt0'], config_bio['simulation']['dt1'], config_bio['simulation']['stepsize_controller']
-    save_steps, max_steps = 50, 16**5
-
-    return signal_onehot, signal_target, y00, t0, t1, dt0, dt1, stepsize_controller, save_steps, max_steps, forward_rates, reverse_rates
-
-
-def prep_cfg(config_bio, input_species):
-
-    config_bio = prepare_config(expand_config(config=config_bio))
-    if config_bio.get('circuit_generation', {}).get('species_count') is not None:
-        assert len(input_species) == config_bio.get('circuit_generation', {}).get(
-            'species_count'), f'Wrong number of input species {input_species}'
-    config_bio.update(expand_model_config(config_bio, {}, input_species))
-    return config_bio
 
 
 def verify(params, rng, decoder,
