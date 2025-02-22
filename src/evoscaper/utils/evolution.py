7d4f2c7adc2f1b815dd55d97a14be29f6419575c
olive004 <olivia.gallup@gmail.com>
2025-02-22 19:27:55 +0000

    add comments

--- src/evoscaper/utils/evolution.py
+++ src/evoscaper/utils/evolution.py
@@ -4,6 +4,14 @@ import jax.numpy as jnp
 
 
 def calculate_ruggedness_from_perturbations(analytic_perturbed, analytic_og, eps):
+    """ Calculate the ruggedness using the perturbations of the analytic landscape.
+    The combined metric is the L2 norm of the perturbation for each unique interaction.
+    
+    Args:
+        analytic_perturbed: The perturbed analytic landscape.
+        analytic_og: The original analytic landscape.
+        eps: The perturbation size.
+    """
 
     dp = (analytic_perturbed - analytic_og) / eps
 
