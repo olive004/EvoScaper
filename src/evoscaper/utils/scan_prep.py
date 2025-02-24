9a4084d2159abac00088ebc9ebab1dd2339dcdaf
olive004 <olivia.gallup@gmail.com>
2025-02-24 15:15:32 +0000

    reorganise

--- src/evoscaper/utils/scan_prep.py
+++ src/evoscaper/utils/scan_prep.py
@@ -26,7 +26,6 @@ jax.config.update('jax_platform_name', 'gpu')
 
 jax.devices()
 
-# jupyter nbconvert --to notebook --execute 03_cvae_multi.ipynb --output=03_cvae_multi_2.ipynb --ExecutePreprocessor.timeout=-1
 
 def keep_equal(df):
     pairs = {
@@ -108,42 +107,3 @@ def expand_df_varying(df_hpos, hpos_to_vary_from_og: dict, hpos_to_vary_together
 
     df_hpos.reset_index(drop=True)
     return df_hpos
-
-
-def main(fn_config):
-
-    top_dir = os.path.join('notebooks', 'data', 'cvae_multi', make_datetime_str())
-    os.makedirs(top_dir, exist_ok=True)
-
-    settings = load_json_as_dict(fn_config)
-    basic_setting, df_hpos = load_basics(settings['hpos_basic'])
-    hpos_to_vary_from_og = load_varying(settings['hpos_to_vary_from_og'])
-    hpos_to_vary_together = load_varying(settings['hpos_to_vary_together'])
-
-    df_hpos = expand_df_varying(df_hpos, hpos_to_vary_from_og, hpos_to_vary_together)
-
-    df_hpos_main = df_hpos #.iloc[61:111]
-
-    fn_config_multisim = os.path.join(top_dir, 'config_multisim.json')
-    config_multisim = {
-        'signal_species': ('RNA_0',),
-        'output_species': ('RNA_2',),
-        'eval_n_to_sample': 1e3,
-        'eval_cond_min': -0.2,
-        'eval_cond_max': 1.2,
-        'eval_n_categories': 2,
-        'filenames_train_config': 'data/raw/summarise_simulation/2024_12_05_210221/ensemble_config_update.json',
-        # 'filenames_train_table': f'{data_dir}/raw/summarise_simulation/2024_12_05_210221/tabulated_mutation_info.csv',
-        'filenames_train_table': 'notebooks/data/simulate_circuits/2025_02_01__00_22_38/tabulated_mutation_info.json'
-    }
-    write_json(config_multisim, fn_config_multisim)
-    cvae_scan_multi(df_hpos_main, fn_config_multisim, top_dir, debug=False)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--fn_config', type=str, default='notebooks/data/configs/cvae_multi/data_scan.json',
-                        help='Path to basic and varyingsettings JSON file')
-    args = parser.parse_args()
-    fn_config = args.fn_config
-    main(fn_config)
