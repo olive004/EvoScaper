0214eb506540816288582225ef0e6180dfd0763f
olive004 <olivia.gallup@gmail.com>
2025-09-23 12:11:54 +0100

    set min learning rate and alpha decay

--- src/evoscaper/utils/optimiser.py
+++ src/evoscaper/utils/optimiser.py
@@ -5,29 +5,31 @@ import optax
 
 def make_optimiser(learning_rate_sched: str = 'cosine_decay',
                    learning_rate = 0.1, epochs: int = 100,
-                   l2_reg_alpha: float = 0.01,
+                   a_decay: float = 0.0,
                    use_warmup: bool = False,
                    warmup_epochs: int = 0,
                    n_batches: int = 1,
-                   method: str = 'sgd'):
+                   method: str = 'sgd',
+                   min_learning_rate=1e-6):
     opt_method = getattr(optax, method)
     if use_warmup:
         warmup_fn = optax.linear_schedule(
-            init_value=0., end_value=learning_rate,
+            init_value=min_learning_rate, end_value=learning_rate,
             transition_steps=warmup_epochs * n_batches)
+            # transition_steps=warmup_epochs)
         cosine_epochs = max(epochs - warmup_epochs, 1)
         cosine_fn = optax.cosine_decay_schedule(
             init_value=learning_rate,
-            decay_steps=cosine_epochs * n_batches)
-        schedule_fn = optax.join_schedules(
+            decay_steps=cosine_epochs * n_batches, 
+            alpha=a_decay)
+        learning_rate_scheduler = optax.join_schedules(
             schedules=[warmup_fn, cosine_fn],
             boundaries=[warmup_epochs * n_batches])
-        optimiser = opt_method(learning_rate=schedule_fn)
     else:
         if learning_rate_sched == 'cosine_decay':
             learning_rate_scheduler = optax.cosine_decay_schedule(
-                learning_rate, decay_steps=epochs, alpha=l2_reg_alpha)
+                learning_rate, decay_steps=epochs, alpha=a_decay)
         else:
             learning_rate_scheduler = learning_rate
-        optimiser = opt_method(learning_rate=learning_rate_scheduler)
+    optimiser = opt_method(learning_rate=learning_rate_scheduler)
     return optimiser
