0aab047119e44a749ec4a3431a5429087d2c2e85
olive004 <olivia.gallup@gmail.com>
2026-02-17 16:02:01 +0000

    vis

--- visualisations/common.py
+++ visualisations/common.py
@@ -3,6 +3,7 @@
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from matplotlib import font_manager
 
 
@@ -15,3 +16,18 @@ def set_theme():
     plt.rcParams['font.sans-serif'] = [font_name]
 
     sns.set_theme(font=font_name, style='white')
+
+
+def get_colors(pal, n):
+    cmap = cm.get_cmap(pal, n)
+    return [cmap(i) for i in range(n)]
+
+
+def make_pastel(color: tuple) -> tuple:
+    r, g, b = color[:3]
+    _alpha = 0.4
+    pastel_rgb = (1 - _alpha + _alpha * r,
+                    1 - _alpha + _alpha * g,
+                    1 - _alpha + _alpha * b)
+    color = (*pastel_rgb, 1.0)
+    return color
